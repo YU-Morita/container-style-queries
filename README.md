2f668c8ba7519051eae1fbb54c5b527a2619cd4e
YU-Morita <yuumorita@jim.titech.ac.jp>
2023-06-26 20:10:26 +0900

    Update: 各内容を拡張

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # container-style-queries
+
+1. Container Size Queries
+2. Container Query Length Units
+3. Container Style Queries for custom propreties
+
+## Style queries
+
+## Scope
+
+Style Queries も `container-name` プロパティで
+Query Container の名前空間を定義できる。
+
+名前空間 Query Container の中だけで、スタイルが完結し、
+その他には影響しない。
--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const themeColorPicker = document.querySelector('#theme-picker')
-const boxContainer = document.querySelector('.box-container');
+// const boxContainer = document.querySelector('.box-container');
+const boxContainer = document.querySelector('body');
 
 themeColorPicker.addEventListener('input', event => {
+  event.preventDefault();
   boxContainer.style.setProperty('--theme', event.target.value);
 })
 
